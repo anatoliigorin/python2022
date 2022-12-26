80889f3430c832bf064d533fd57eb78ce89c4a29
Anatolii Gorin <92159095+anatoliigorin@users.noreply.github.com>
2022-12-26 18:31:18 +0500

    Update laba1.py
    Поменял иерархию классов. Отказался от функций распечатки и get_info. Вместо стека для импорта и экспорта стал использовать party

--- laba1.py
+++ laba1.py
@@ -1,103 +1,218 @@
-# coding=windows-1251
+from ftplib import parse150
+from array import *
 import json
-class Item:
-    name = "Undefined"
-    inventoryTab = "Items"
 
-    def __init__(self, name):
-        self.name = name
 
-    def get_info(self):
-        print(f"Название: {self.name}\nВкладка инвентаря: {self.inventoryTab}\n")
+class Weapon:
+    def __init__(self, *args):
+        if (len(args) == 0):
+            self.name = "unknown weapon"
+            self.damage = 0
+            self.price = 0        
+            self.description = "Empty description"
+        elif (len(args) == 1):
+            self.name = args[0]["name"]
+            self.damage = args[0]["damage"]
+            self.price = args[0]["price"]
+            self.description = args[0]["description"]
+        elif (len(args) == 3):
+            self.name = args[0]
+            self.damage = args[1]
+            self.price = args[2] 
+            self.description = "Empty description"
+        elif (len(args) == 4):
+            self.name = args[0]
+            self.damage = args[1]
+            self.price = args[2]        
+            self.description = args[3]  
 
-class Tool(Item):
-    hardness = 0
-    inventoryTab = "Tools"
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
-    def __init__(self, name, hardness):
-        super().__init__(name)
-        if hardness >= 0:
-            self.hardness = hardness
+    def import_file(self, file_name):
+        with open(file_name, "r") as the_file:
+            raw_weapon = json.load(the_file)
+            self.name = raw_weapon["name"]
+            self.damage = raw_weapon["damage"]
+            self.price = raw_weapon["price"]
+            self.description = raw_weapon["description"]
+
+    def export_file(self, file_name):
+        with open(file_name, "w") as the_file:
+            the_file.write(self.to_json())
+
+class Shield:
+    def __init__(self, *args):
+        if (len(args) == 0):
+            self.name = "unknown shield"
+            self.defence = 0
+            self.price = 0        
+            self.description = "Empty description"
+        elif (len(args) == 1):
+            self.name = args[0]["name"]
+            self.defence = args[0]["defence"]
+            self.price = args[0]["price"]
+            self.description = args[0]["description"]
+        elif (len(args) == 3):
+            self.name = args[0]
+            self.defence = args[1]
+            self.price = args[2]  
+            self.description = "Empty description"
+        elif (len(args) == 4):
+            self.name = args[0]
+            self.defence = args[1]
+            self.price = args[2]        
+            self.description = args[3]  
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+    def import_file(self, file_name):
+        with open(file_name, "r") as the_file:
+            raw_shield = json.load(the_file)
+            self.name = raw_shield["name"]
+            self.defence = raw_shield["defence"]
+            self.price = raw_shield["price"]
+            self.description = raw_shield["description"]
+
+    def export_file(self, file_name):
+        with open(file_name, "w") as the_file:
+            the_file.write(self.to_json())
+
+class Armor:
+    def __init__(self, *args):
+        if (len(args) == 0):
+            self.name = "unknown armor"
+            self.defence = 0
+            self.price = 0        
+            self.description = "Empty description"
+        elif (len(args) == 1):
+            self.name = args[0]["name"]
+            self.defence = args[0]["defence"]
+            self.price = args[0]["price"]
+            self.description = args[0]["description"]
+        elif (len(args) == 3):
+            self.name = args[0]
+            self.defence = args[1]
+            self.price = args[2] 
+            self.description = "Empty description"
+        elif (len(args) == 4):
+            self.name = args[0]
+            self.defence = args[1]
+            self.price = args[2]        
+            self.description = args[3]        
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+    def import_file(self, file_name):
+        with open(file_name, "r") as the_file:
+            raw_armor = json.load(the_file)
+            self.name = raw_armor["name"]
+            self.defence = raw_armor["defence"]
+            self.price = raw_armor["price"]
+            self.description = raw_armor["description"]
+
+    def export_file(self, file_name):
+        with open(file_name, "w") as the_file:
+            the_file.write(self.to_json())
+
+class Character:
+    def __init__(self, *args):
+        if len(args) == 0:
+            self.name = "unknown character"
+            self.health = 100        
+            self.weapon = Weapon()
+            self.shield = Shield()
+            self.armor = Armor() 
+        elif (len(args) == 1):
+            self.name = args[0]["name"]
+            self.health = args[0]["health"]
+            self.weapon = Weapon(args[0]["weapon"])
+            self.shield = Shield(args[0]["shield"])
+            self.armor = Armor(args[0]["armor"])
+        elif (len(args) == 5):
+            self.name = args[0]
+            self.health = args[1]        
+            self.weapon = args[2]
+            self.shield = args[3]
+            self.armor = args[4]          
+
+    def die(self, damage = 0):
+        print(self.name + " received " + damage + " and died!")
+        del self
+
+    def receive_damage(self, damage = 0):
+        if (damage > self.health):
+            self.die(damage)
         else:
-            print("Недопустимая прочность")
+            self.health -= damage
 
-    def get_info(self):
-        print(f"Название: {self.name}\nВкладка инвентаря: {self.inventoryTab}\nПрочность: {self.hardness}\n")
+    def attack(self, target):
+        dmg = self.weapon.damage - target.armor.defence - target.left_hand.defence
+        if dmg < 0:
+            dmg = 0
+        target.Receive_damage(dmg)
 
-class Weapon(Item):
-    strength = 0
-    inventoryTab = "Weapons"
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
-    def __init__(self, name, hardness, strength):
-        super().__init__(name)
-        self.hardness = hardness
-        if strength >= 0:
-            self.strength = strength
-        else:
-            print("Недопустимая сила")
+    def print(self):
+        print(self.name + ": hp = " + str(self.health) + ", weapon_dmg = " + str(self.weapon.damage) + ", shield_defence = " + str(self.shield.defence) + ", armor_defence = " + str(self.armor.defence))
 
-    def get_info(self):
-        print(f"Название: {self.name}\nВкладка инвентаря: {self.inventoryTab}\nПрочность: {self.hardness}\nСила: {self.strength}\n")
+    def import_file(self, file_name):
+        with open(file_name, "r") as the_file:
+            raw_character = json.load(the_file)
+            self.name = raw_character["name"]
+            self.health = raw_character["health"]
+            self.weapon = Weapon(raw_character["weapon"])
+            self.shield = Shield(raw_character["shield"])
+            self.armor = Armor(raw_character["armor"])
 
-class Bow(Weapon):
-    stringTension = 0
-    hardness = 64
-    strength = 13
-    name = "Bow"
+    def export_file(self, file_name):
+        with open(file_name, "w") as the_file:
+            the_file.write(self.to_json())
 
-    def __init__(self, stringTension):
-        super().__init__(self.name, self.hardness, self.strength)
-        if stringTension >= 0:
-            self.stringTension = stringTension
-        else:
-            print("Недопустимое натяжение тетивы")
+class Party:
+    def __init__(self):        
+        self.members = []    
 
-    def get_info(self):
-        print(f"Название: {self.name}\nВкладка инвентаря: {self.inventoryTab}\nПрочность: {self.hardness}\nСила: {self.strength}\nНатяжение: {self.stringTension}\n")
+    def add_member(self, char):        
+        self.members.append(char)
 
-class Block(Item):
-    inventoryTab = "Blocks"
-    isDestructable = True
-    isFluid = False
+    def delete_member(self, char):
+        if (self.memebers.find(char) >= 0):            
+            self.members.pop(self.members.find(char))
 
-    def __init__(self, name, isDestructable, isFluid):
-        super().__init__(name)
-        self.isDestructable = isDestructable
-        if isDestructable == False:
-            self.isFluid = isFluid
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
-    def get_info(self):
-        print(f"Название: {self.name}\nВкладка инвентаря: {self.inventoryTab}\nРазрушаемость: {self.isDestructable}\nЖидкий: {self.isFluid}\n")
+    def print(self):
+        for item in self.members:
+            item.print()
+
+    def import_file(self, file_name):
+        with open(file_name, "r") as the_file:
+            raw_party = json.load(the_file)
+            self.members = []
+            for member in raw_party["members"]:
+                self.add_member(Character(member))
+
+    def export_file(self, file_name):
+        with open(file_name, "w") as the_file:
+            the_file.write(self.to_json())
 
 
-bow = Bow(14)
-bow.get_info()
+example_party = Party()
+example_party.add_member(Character("example_char1", 90, Weapon("Frostmourne", 100, 1000), Shield("The Door", 10, 2000), Armor("Havel armor", 30, 1500)))
+example_party.add_member(Character("example_char2", 80, Weapon("Shadowmourne", 50, 800), Shield("Great Protector", 8, 1000), Armor("Loincloth", 20, 1000)))
 
-stone = Block("Камень", True, False)
-stone.get_info()
+print("Party sent to JSON file:")
+example_party.print()
 
-sword = Weapon("Меч", 64, 15)
-sword.get_info()
+example_party.export_file("the_file.json")
 
-pickaxe = Tool("Кирка", 1024)
-pickaxe.get_info()
-
-data = {
-    "bow":{
-        "name": bow.name,
-        "hardness" : bow.hardness
-        },
-    "stone":{
-        "name": stone.name,
-        "tab": stone.inventoryTab,
-        "isDestructable": stone.isDestructable,
-        "isFluid": stone.isFluid
-        }
-    }
-with open("data_file.json", "w") as write_file:
-    json.dump(data, write_file)
-
-with open("data_file.json", "r") as read_file:
-    new_data = json.load(read_file)
-
-print(new_data)
+imported_party = Party()
+imported_party.import_file("the_file.json")
+print("\nParty received from JSON file:")
+imported_party.print()
